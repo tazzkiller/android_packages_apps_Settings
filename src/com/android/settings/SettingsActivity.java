c928392b1c4158fd94d0b74ef529dbc692e7112e
darkobas <darkobas@gmail.com>
2014-12-01 14:54:20 +0100

    fix mismerge
    Change-Id: I6cfd5fdc489c3e1cece8674bb9c1f34a62578f41

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1202,6 +1202,8 @@ public class SettingsActivity extends Activity
                     boolean hasDeviceKeys = getResources().getInteger(
                             com.android.internal.R.integer.config_deviceHardwareKeys) != 0;
                     if (!hasDeviceKeys) {
+                        removeTile = true;
+                    }
                 } else if (id == R.id.supersu_settings) {
                     // Embedding into Settings is supported from SuperSU v1.85 and up
                     boolean supported = false;
