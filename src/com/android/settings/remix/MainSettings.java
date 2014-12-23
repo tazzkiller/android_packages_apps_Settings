f8a8a6262ff914925ae8631d49a7b5f257e869ca
XXMrHyde <xxmrhyde@gmx.co.uk>
2014-12-23 06:41:49 +0100

    iLaunch LockClock preferences from Settings
    Change-Id: I7c0143db2765845ebbfd75f0c811f2fa5063ef42

--- src/com/android/settings/remix/MainSettings.java
+++ src/com/android/settings/remix/MainSettings.java
@@ -18,6 +18,7 @@ package com.android.settings.remix;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -29,11 +30,28 @@ import com.android.settings.SettingsPreferenceFragment;
 import java.util.List;
 
 public class MainSettings extends SettingsPreferenceFragment {
+    private static final String KEY_LOCK_CLOCK = "lock_clock";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.remix_main_settings);
+        // Remove the lock clock preference if its not installed
+        if (!isPackageInstalled("com.cyanogenmod.lockclock")) {
+            removePreference(KEY_LOCK_CLOCK);
+        }
+
+    }
+    private boolean isPackageInstalled(String packageName) {
+        PackageManager pm = getPackageManager();
+        boolean installed = false;
+        try {
+           pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+           installed = true;
+        } catch (PackageManager.NameNotFoundException e) {
+           installed = false;
+        }
+        return installed;
     }
 }
